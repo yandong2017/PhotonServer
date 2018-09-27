a152f9912d41e6de5d69dd6cc00ecded4b1e3afc
闫栋 <dong.yan@reeed.org>
2018-09-27 16:28:19 +0800

    服务器基础架构

---
+++ ServerGame/ServerGame/ClientPeer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+ 
+namespace ServerGame
+{
+    class ClientPeer : Photon.SocketServer.ClientPeer
+    {
+        public ClientPeer(InitRequest ir) : base(ir) { }
+
+        //该客户端断开连接
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+
+        }
+
+        //该客户端出操作请求
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            /*****解析客户端请求数据*****/
+            ReadRequestData(operationRequest);
+            /****************************/
+
+            /*****向客户端发送响应*****/
+            OperationResponse operationResponse = new OperationResponse();
+            operationResponse.OperationCode = 0;
+            //数据包
+            operationResponse.Parameters = new Dictionary<byte, object> { { 1, "数据提交成功" } };
+            //向客户端发送请求响应
+            SendOperationResponse(operationResponse, sendParameters);
+            /**************************/
+
+            /*****向客户端发送事件*****/
+            EventData eventData = new EventData(0, new Dictionary<byte, object> { { 1, "服务器事件" } });
+            SendEvent(eventData, new SendParameters());
+            /**************************/
+        }
+
+        private void ReadRequestData(OperationRequest operationRequest)
+        {
+            int operationCode = operationRequest.OperationCode;
+            switch (operationCode)
+            {
+                case 0:
+                    Dictionary<byte, object> requestData = operationRequest.Parameters;
+                    object value1 = requestData.FirstOrDefault(q => q.Key == 1).Value;
+                    object value2 = requestData.FirstOrDefault(q => q.Key == 2).Value;
+
+                    ServerGame.LOG.Warn("数据一：" + value1 + "数据二：" + value2);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
---
+++ ServerGame/ServerGame/ServerGame.cs
@@ -0,0 +1,49 @@
+﻿using ExitGames.Logging;
+using ExitGames.Logging.Log4Net;
+using log4net.Config;
+using Photon.SocketServer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerGame
+{
+    public class ServerGame : Photon.SocketServer.ApplicationBase
+    {
+        //单例模式
+        public static ILogger LOG = LogManager.GetCurrentClassLogger();
+        //当有客户端接入时候调用
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new ClientPeer(initRequest);
+        }
+        //当框架启动时候调用
+        protected override void Setup()
+        {
+            //设置配置文件属性
+            log4net.GlobalContext.Properties["Photon:ApplicationLogPath"] = Path.Combine(Path.Combine(this.ApplicationRootPath, "ServerGame"), "log");//设置日志文件存储目录
+
+            //日志配置文件
+            FileInfo logConfigFileInfo = new FileInfo(Path.Combine(this.BinaryPath, "log4net.config"));
+            if (logConfigFileInfo.Exists)//配置文件存在
+            {
+                //设置Photon日志插件为Log4Next
+                LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+                //Log4Next这个插件读取配置文件
+                XmlConfigurator.ConfigureAndWatch(logConfigFileInfo);
+            }
+
+            LOG.Info("服务器初始化完成");
+        }
+
+        //当框架停止时候调用
+        protected override void TearDown()
+        {
+        }
+    }
+
+
+}
